e5b6fdbb8a513877a7ffc99408fb6501a80ac4b0
Chun <cw1753@nyu.edu>
2019-02-01 00:15:59 -0500

    .

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import GoogleLogin from 'react-google-login';
+// import GithubLogin from 'react-github-login';
 require('dotenv').config();
 
 export default class Login extends Component {
@@ -23,15 +24,38 @@ export default class Login extends Component {
         this.props.history.push('/');
     }
 
+    responseGithub = (res) =>{
+        fetch("https://github.com/login/oauth/authorize?client_id=" + process.env.REACT_APP_GITHUB_CLIENT_ID, {
+            method: 'GET',
+            mode: 'no-cors'
+        }).then( (res) => {
+            if(res.status >= 400) {
+                throw new Error(res.error);
+            }
+            return res.json();
+        }).then( (data) => {
+            console.log(data);
+        }).catch( (err) => {
+            console.log("Error: ", err);
+        })
+        // this.props.history.push('/');
+    }
+
     render() {
         return (
             <div>
                 <GoogleLogin
-                    client_id={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                    clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
                     buttonText="Google Login"
                     onSuccess={this.responseGoogle}
                     onFailure={this.responseGoogle}
                 />
+                {/* <GithubLogin
+                    clientId={process.env.REACT_APP_GITHUB_CLIENT_ID}
+                    onSuccess={this.responseGithub}
+                    onFailure={this.responseGithub}
+                /> */}
+                <button onClick={this.responseGithub}>Github Login</button>
             </div>
         )
     }
